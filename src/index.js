39f0c064b864898253f8f5d2389c4fd36d558ff0
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-24 03:27:25 +0000

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import createStore from 'redux';
-import { Providr } from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducers from './reducers';
 import App from './components/App';
 
@@ -11,4 +11,4 @@ ReactDom.render(
     <Provider store={store}>    
       <App />
     </Provider>,document.getElementById("root")
-  )
+  );
